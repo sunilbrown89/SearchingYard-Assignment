1b1bea94b6b3a956d3e7e27eb086f4b0397c66b1
Sunil Sahu <sunil.rock.9026040@gmail.com>
2023-02-16 03:05:56 +0530

    path change

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
-    const URL=`mongodb+srv://${username}:${password}@cluster0.u8z0f.mongodb.net/blogXYZ?retryWrites=true&w=majority`
+    const URL=`mongodb+srv://${username}:${password}@cluster0.u8z0f.mongodb.net/blogYard?retryWrites=true&w=majority`
     try {
         await mongoose.connect(URL, { useNewUrlParser: true })
         console.log('Database connected successfully');
